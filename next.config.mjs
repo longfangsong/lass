3ae9311d906d41bbfad71809e6ed54c6b349dea7
longfangsong <longfangsong@icloud.com>
2024-12-04 23:07:46 +0100

    Service worker

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+import withSerwistInit from "@serwist/next";
 
 // Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
 // (when running the application with `next dev`), for more information see:
@@ -7,7 +8,14 @@ if (process.env.NODE_ENV === 'development') {
   await setupDevPlatform();
 }
 
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: "src/app/sw.ts",
+  swDest: "public/sw.js",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withSerwist(nextConfig);
