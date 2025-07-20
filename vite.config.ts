92eb455af8f9c8836678570827ac01a9added049
longfangsong <longfangsong@icloud.com>
2025-07-20 23:08:09 +0200

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare()],
-})
+  plugins: [react(), tailwindcss(), cloudflare()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
--- worker/index.ts
+++ worker/index.ts
@@ -1,12 +1,12 @@
 export default {
-  fetch(request) {
+  fetch(request, env: Env) {
     const url = new URL(request.url);
-
+    console.log(env);
     if (url.pathname.startsWith("/api/")) {
       return Response.json({
         name: "Cloudflare",
       });
     }
-		return new Response(null, { status: 404 });
+    return new Response(null, { status: 404 });
   },
 } satisfies ExportedHandler<Env>;
